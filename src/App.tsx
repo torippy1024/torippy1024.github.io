12ded9e5fa3a1e9a9592b9740b184c0ab03ea607
h.toriyama <toriyamahiroto1128@gmail.com>
2022-10-07 00:35:14 +0900

    初期設定

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import TopPage from './pages/TopPage';
 function App() {
   return (
     <div>
-      <BrowserRouter basename='repository-name'>
+      <BrowserRouter basename=''>
         <Routes>
           <Route index element={<TopPage />} />
         </Routes>
